cc7c90228d0df2a0f1235f175c89bb8a16fc507d
owiekelvin3-cpu <owiekelvin3@gmail.com>
2026-04-12 15:31:41 +0300

    Restore JambSuccessGuide component - deployment working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import TestComponent from './TestComponent';
+import JambSuccessGuide from './JambSuccessGuide';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TestComponent />
+    <JambSuccessGuide />
   </React.StrictMode>
 );
 
